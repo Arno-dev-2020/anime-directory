bf6f25ba2bf70d846c63a6e56c9f4ca6f8ada098
Arno-dev-2020 <64438565+Arno-dev-2020@users.noreply.github.com>
2025-01-11 20:48:22 +0000

    Add React application files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Anime Directory</h1>
+      <p>Welcome to your anime discovery platform!</p>
+    </div>
+  )
+}
+
+export default App 
